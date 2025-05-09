198a4941ecfe52d3d8a1a3d688e312a7e24feb1c
James Black <jmblack@waimea.school.nz>
2025-05-09 15:07:18 +1200

    g

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,17 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
+
+
+@app.get("/")
+def home():
+    return render_template("pages/home.jinja")
+
+@app.get("/test/")
+def test():
+    return "<h1>Testing... Yo Mamma</h1>"
+
+@app.get("/umIDK/")
+def umIDK():
+    return "<h1>Testing... My brain cells</h1>"
